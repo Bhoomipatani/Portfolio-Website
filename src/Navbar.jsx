8d5956bf7f9cf97b042da3f3b2137e4e3d7673ae
Bhoomi Patani <bhoomipatani211@gmail.com>
2024-08-23 14:09:53 +0530

    fixing navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,17 +10,15 @@ function Navbar() {
 
   return (
     <nav className="bg-transparent z-50 relative">
-      <div className="flex items-center justify-between p-5">
+      <div className="container mx-auto flex items-center justify-between p-5">
         {/* Logo */}
-        <div className="flex flex-shrink-0">
-          <a href="/" className="pl-12">
-            <img src={Logo} className="w-12 h-10" alt="Logo" />
-          </a>
-        </div>
+        <a href="/" className="flex-shrink-0">
+          <img src={Logo} className="w-12 h-10" alt="Logo" />
+        </a>
 
         {/* Toggle Button for Mobile */}
         <button
-          className="md:hidden p-2 text-white"
+          className="lg:hidden p-2 text-white"
           onClick={toggleNavbar}
         >
           <svg
@@ -40,35 +38,73 @@ function Navbar() {
         </button>
 
         {/* Navbar Links */}
-        <div className={`md:flex md:items-center ${isOpen ? "block" : "hidden"} w-full mx-8 md:w-auto`}>
-          <ul className="flex flex-col md:flex-row md:space-x-8 text-white text-sm md:ml-auto">
-            <li className="py-2 md:py-0">
-              <a className="transition-colors duration-300 hover:text-pink" href="/#skills">
-                SKILLS
-              </a>
-            </li>
-            <li className="py-2 md:py-0">
-              <a className="transition-colors duration-300 hover:text-pink" href="/#about">
-                ABOUT
-              </a>
-            </li>
-            <li className="py-2 md:py-0">
-              <a className="transition-colors duration-300 hover:text-pink" href="/#experience">
-                EXPERIENCE
-              </a>
-            </li>
-            <li className="py-2 md:py-0">
-              <a className="transition-colors duration-300 hover:text-pink" href="/#projects">
-                PROJECTS
-              </a>
-            </li>
-            <li className="py-2 md:py-0">
-              <a className="transition-colors duration-300 hover:text-pink" href="/#education">
-                EDUCATION
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul
+          className={`lg:flex lg:items-center lg:space-x-8  lg:static fixed left-0 top-0 w-2/3 h-full lg:w-auto lg:bg-transparent bg-bg_color_2 text-white p-8 lg:p-0 lg:pr-8 transition-transform duration-300 ease-in-out ${
+            isOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
+          }`}
+        >
+          <li className="lg:hidden mb-4">
+            <button
+              className="absolute top-4 right-4 p-2 text-white"
+              onClick={toggleNavbar}
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+          <li>
+            <a
+              className="block text-sm mt-2 text-white transition-colors duration-300 hover:text-pink"
+              href="/#skills"
+            >
+              SKILLS
+            </a>
+          </li>
+          <li>
+            <a
+              className="block text-sm mt-2 text-white transition-colors duration-300 hover:text-pink"
+              href="/#about"
+            >
+              ABOUT
+            </a>
+          </li>
+          <li>
+            <a
+              className="block text-sm mt-2 text-white transition-colors duration-300 hover:text-pink"
+              href="/#experience"
+            >
+              EXPERIENCE
+            </a>
+          </li>
+          <li>
+            <a
+              className="block text-sm mt-2 text-white transition-colors duration-300 hover:text-pink"
+              href="/#projects"
+            >
+              PROJECTS
+            </a>
+          </li>
+          <li>
+            <a
+              className="block text-sm mt-2 text-white transition-colors duration-300 hover:text-pink"
+              href="/#education"
+            >
+              EDUCATION
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
